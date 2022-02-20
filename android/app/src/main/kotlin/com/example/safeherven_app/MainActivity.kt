46870a1d26b40f517a7f99293089cfe707baff1b
Ida Delphine <idadelm@gmail.com>
2022-02-20 10:40:59 +0100

    Created base app

---
+++ android/app/src/main/kotlin/com/example/safeherven_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.safeherven_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Text('Hello World'),
+  ));
+}
+
