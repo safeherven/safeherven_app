9066b863bcfc98179881288eb80ad1e3907e6873
Ida Delphine <idadelm@gmail.com>
2022-02-22 14:51:43 +0100

    Created the chatbot interface with dialogflow

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,18 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:safeherven_app/shared/drawer.dart';
+import 'package:safeherven_app/shared/chat.dart';
 import 'package:safeherven_app/shared/bottom.dart';
 
-class ChatScreen extends StatelessWidget {
+
+class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
 
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Let's Talk"),),
-      // drawer: const MenuDrawer(),
       bottomNavigationBar: const MenuBottom(),
       body: const Center(
+        child: Chat(),
       ),
     );
   }
 }
+
+
+// class ChatScreen extends StatelessWidget {
+//   const ChatScreen({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: const Text("Let's Talk"),),
+//       bottomNavigationBar: const MenuBottom(),
+//       body: const Center(
+//       ),
+//     );
+//   }
+// }
