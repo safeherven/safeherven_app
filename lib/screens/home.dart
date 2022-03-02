21e9dae198c2647fc61dbbde08201b2930700d24
Ida Delphine <idadelm@gmail.com>
2022-03-02 10:46:49 +0100

    Added content to home page
    Also added notifications and settings icon to the appbar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -9,11 +9,74 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Safe Herven'),),
+      appBar: AppBar(
+        centerTitle: true,
+        title: IconButton(
+          onPressed: null,
+          icon: Image.asset('assets/images/logos/white.png'),
+          iconSize: 50,
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(
+              Icons.notifications,
+              color: Colors.white,
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(
+              Icons.settings,
+              color: Colors.white,
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
       drawer: const MenuDrawer(),
       bottomNavigationBar: const MenuBottom(),
-      body: const Center(
-        child: Text('Safe Herven!'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(20),
+            width: 200,
+            child: Column(
+              children: const <Widget>[
+                Text(
+                  'Hello Firstname',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'Welcome back! \nWe are here for you as always to help.',
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                children: const <Widget>[
+                  Text(
+                    'Need Help?',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'Explore the menu to find places or  create danger alerts to keep others around you safe. The chatbot is there for you if you just wanna talk and let it all out.',
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
       floatingActionButton:
       FloatingActionButton(child: const Icon(Icons.chat), onPressed: () {
